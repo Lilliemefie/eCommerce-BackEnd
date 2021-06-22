069ab4241e022ca9953dcccbc00d7e9c4da7f4a8
lillie <lillie@Lillies-MacBook-Pro.local>
2021-06-22 16:45:32 -0700

    added through producttag in index.js

--- models/index.js
+++ models/index.js
@@ -14,14 +14,20 @@ Category.hasMany(Product, {
 })
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
-  through: ProductTag,
-  foreignKey: 'tag_id',
-})
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  as: 'Product_tags'
+});
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
-  through: ProductTag,
-  foreignKey: 'product_id',
-})
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  as: 'Tag_products'
+});
 
 
 module.exports = {
