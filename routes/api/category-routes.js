dfb2ddc43122e8669d13fc637522bd3a527d8e8b
lillie <lillie@Lillies-MacBook-Pro.local>
2021-06-22 08:52:07 -0700

    added find one category by id Route

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -7,18 +7,30 @@ router.get('/', async (req, res) => {
   // find all categories
   // be sure to include its associated Products
   try{
-    const categoryCardData = await Category.findAll({
+    const categoryData = await Category.findAll({
       include: [{model: Product}],
     });
-    res.status(200).json(categoryCardData);
+    res.status(200).json(categoryData);
   } catch (err){
     res.status(500).json(err);
   }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  try {
+    const categoryData = await Category.findByPk(req.params.id, {
+      include: [{ model: Product}],
+    });
+    if (!categoryData){
+      res.status(404).json({ message: "No this category's id found"});
+      return;
+    }
+    res.status(200).json(categoryData);
+  } catch (err){
+    res.status(500).json(err);
+  }
 });
 
 router.post('/', (req, res) => {
