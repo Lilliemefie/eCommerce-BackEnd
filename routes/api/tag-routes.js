b5c2c8a81c014f0dc572d1d5ac743683a0deca69
lillie <lillie@Lillies-MacBook-Pro.local>
2021-06-23 01:15:14 -0700

    fixed lin 27 for both tag-routes.js and product-routes.js

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -23,7 +23,7 @@ router.get('/:id', async (req, res) => {
   try {
     const tagData = await Tag.findByPk(req.params.id, {
       // be sure to include its associated Product data
-      include: [{ model: Product }],
+      include: [{ model: Product, through: ProductTag, as: 'products_data'}],
     });
     if (!tagData) {
       res.status(404).json({ messgae: 'No tag found with that ID!'});
